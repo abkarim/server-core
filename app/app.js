597f7285f35db7cb386b486265b92c259c2993b1
unknown <akskarim17@gmail.com>
2023-02-14 16:54:23 +0600

    middleware added

--- app/app.js
+++ app/app.js
@@ -1,4 +1,24 @@
 const express = require("express");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+
 const app = express();
+
+/**
+ * Set HTTP headers
+ */
+app.use(helmet());
+
+app.use(express.urlencoded());
+
+/**
+ * Parse json data
+ */
+app.use(express.json());
+
+/**
+ * Sanitize user data to protect NoSQL injection
+ */
+app.use(mongoSanitize());
 
 module.exports = app;
