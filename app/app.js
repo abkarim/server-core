e62208f13ad539aa9dc82e2e2cd38b8e32bd0277
unknown <akskarim17@gmail.com>
2023-03-07 16:51:24 +0600

    static folder options added

--- app/app.js
+++ app/app.js
@@ -1,24 +1,33 @@
+const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 
+const api = require("./api");
+
 const app = express();
+
+const CURRENT_DIRECTORY = __dirname;
 
 /**
  * Set HTTP headers
  */
 app.use(helmet());
 
-app.use(express.urlencoded());
-
-/**
- * Parse json data
- */
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(mongoSanitize());
 
 /**
- * Sanitize user data to protect NoSQL injection
+ * Expose a public path
  */
-app.use(mongoSanitize());
+app.use(
+  express.static(path.join(CURRENT_DIRECTORY, "../public"), {
+    dotfiles: "ignore",
+    index: false,
+  })
+);
+
+app.use("/api", api);
 
 module.exports = app;
